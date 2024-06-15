3ba622cede4515bff4bcb1e67dd1f3aebce1ae6f
Kris Jenkins <krisajenkins@gmail.com>
2024-06-15 10:04:11 +0200

    Big refactoring. Biggest change is incremental rendering.

--- src/main.rs
+++ src/main.rs
@@ -3,81 +3,113 @@ use std::collections::HashSet;
 use std::thread::sleep;
 use std::time::Duration;
 
-const X_MAX: i16 = 80;
+const X_MAX: i16 = 200;
 const Y_MAX: i16 = 60;
+const SLEEP_TIME: u64 = 10;
+
+enum CellState {
+    Live,
+    Dead,
+}
+
+type Coord = (i16, i16);
+type World = HashSet<Coord>;
+type Change = (Coord, CellState);
+
+struct Update {
+    new_world: World,
+    changes: Vec<Change>,
+}
 
 fn main() {
     let mut world = HashSet::new();
 
+    // Fill the world with random cells.
     let mut rng = rand::thread_rng();
-
-    for _ in 0..500 {
+    for _ in 0..(X_MAX * Y_MAX / 2) {
         let x = rng.gen_range(0..X_MAX);
         let y = rng.gen_range(0..Y_MAX);
         world.insert((x, y));
     }
 
     loop {
-        let new_world = iterate(X_MAX, Y_MAX, &world);
-        display(X_MAX, Y_MAX, &new_world, &world);
-        sleep(Duration::from_millis(50));
-        world = new_world;
+        let update = step_world_forward(X_MAX, Y_MAX, &world);
+        display(&update);
+        world = update.new_world;
+        sleep(Duration::from_millis(SLEEP_TIME));
     }
 }
 
-fn display(
-    x_max: i16,
-    y_max: i16,
-    new_world: &HashSet<(i16, i16)>,
-    old_world: &HashSet<(i16, i16)>,
-) {
-    print!("\x1B[2J\x1B[1;1H");
+// NOTE: Ansi Escape Codes: https://en.wikipedia.org/wiki/ANSI_escape_code
+//
+// `CSI 17;5H` is the control code that moves the cursor to 17,5. The screen is 1-indexed from
+// top-left.
+const ESCAPE: &str = "\x1B";
+const CLEAR_SCREEN: &str = "[2J";
+const GO_TO_TOP_LEFT: &str = "[1;1H";
 
-    for y in 0..y_max {
-        for x in 0..x_max {
-            let point = (x, y);
-            if new_world.contains(&point) {
-                print!("O");
-            } else if old_world.contains(&point) {
-                print!(".");
-            } else {
-                print!(" ");
-            }
+fn display(update: &Update) {
+    let mut s: String = String::new();
+
+    s += &format!("{}{}", ESCAPE, CLEAR_SCREEN);
+    s += &format!("{}{}", ESCAPE, GO_TO_TOP_LEFT);
+
+    for ((x, y), state) in update.changes.iter() {
+        s += &format!("{}[{};{}H", ESCAPE, y, x);
+
+        s += match state {
+            CellState::Live => "X",
+            CellState::Dead => " ",
+        };
+    }
+    print!("{}", s);
+}
+
+fn count_neighbours(x: i16, y: i16, old_world: &World) -> i16 {
+    let mut n = 0;
+
+    for (dx, dy) in [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ] {
+        if old_world.contains(&(x + dx, y + dy)) {
+            n += 1;
         }
-        println!();
     }
+
+    return n;
 }
 
-fn iterate(x_max: i16, y_max: i16, old_world: &HashSet<(i16, i16)>) -> HashSet<(i16, i16)> {
+fn step_world_forward(x_max: i16, y_max: i16, old_world: &World) -> Update {
     let mut new_world = HashSet::new();
+    let mut changes = Vec::new();
 
     for x in 0..x_max {
         for y in 0..y_max {
-            let mut n = 0;
+            let n = count_neighbours(x, y, old_world);
 
-            for (dx, dy) in [
-                (-1, -1),
-                (0, -1),
-                (1, -1),
-                (-1, 0),
-                (1, 0),
-                (-1, 1),
-                (0, 1),
-                (1, 1),
-            ] {
-                if old_world.contains(&(x + dx, y + dy)) {
-                    n += 1;
+            match (n, old_world.contains(&(x, y))) {
+                (2, true) => {
+                    new_world.insert((x, y));
                 }
-            }
-
-            if n == 2 && old_world.contains(&(x, y)) {
-                new_world.insert((x, y));
-            }
-            if n == 3 {
-                new_world.insert((x, y));
-            }
+                (3, true) => {
+                    new_world.insert((x, y));
+                }
+                (3, false) => {
+                    new_world.insert((x, y));
+                    changes.push(((x, y), CellState::Live))
+                }
+                (_, true) => changes.push(((x, y), CellState::Dead)),
+                (_, _) => (),
+            };
         }
     }
 
-    new_world
+    return Update { new_world, changes };
 }
